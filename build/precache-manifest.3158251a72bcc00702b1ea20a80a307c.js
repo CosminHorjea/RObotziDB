7f92384e03e19007305655213f38eb71d8445493
Cosmin Horjea <horjeacosmin@yahoo.com>
2019-10-05 23:57:44 +0300

    removed build from gitignore

---
+++ build/precache-manifest.3158251a72bcc00702b1ea20a80a307c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c34a4ca5c1e0d61b5dfcc901673432",
+    "url": "/index.html"
+  },
+  {
+    "revision": "356e414aa6e4a9bfa278",
+    "url": "/static/css/main.66f000e4.chunk.css"
+  },
+  {
+    "revision": "1510ec878dd042975d31",
+    "url": "/static/js/2.7250f918.chunk.js"
+  },
+  {
+    "revision": "356e414aa6e4a9bfa278",
+    "url": "/static/js/main.ada20d28.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "50b82309dcd8caa24a3b7819dd63e7a3",
+    "url": "/static/media/logoCM.50b82309.png"
+  }
+]);
